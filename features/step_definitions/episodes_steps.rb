fd0c59e74c2b70f33758ed9bdc3f51f1c1eefda4
Marcel Fahle <m.fahle@gmail.com>
2013-09-27 03:44:50 +0200

    second scenario is passing too with hardcoded HTML

--- features/step_definitions/episodes_steps.rb
+++ features/step_definitions/episodes_steps.rb
@@ -9,10 +9,18 @@ Then(/^I should see that there are no episodes published$/) do
   page.should have_content("There are no published episodes.")
 end
 
+Episode = Struct.new :status, :title
 Given(/^there are some episodes published$/) do
-  pending # express the regexp above with the code you wish you had
+  @episodes = [
+    Episode.new("published", "Episode 1"),
+    Episode.new("published", "Episode 2")
+  ]
 end
 
 Then(/^I should see the published episodes sorted by date$/) do
-  pending # express the regexp above with the code you wish you had
+  @episodes.each do |episode|
+    within(".#{episode.status}") do
+      page.should have_css(".episode", text: episode.title)
+    end
+  end
 end
