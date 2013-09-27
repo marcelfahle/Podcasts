21e78decd792b82771725cdb65aeb3398a57f8fa
Marcel Fahle <m.fahle@gmail.com>
2013-09-27 03:13:38 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Podcasts::Application.config.secret_key_base = 'ffc04cb09e1adaaff11a8591cddd13a60332e8d24ac846ef206eea1e6c7c5f0333c4187be3837da434f5e9cbfab738b83e5431a962e38f7e4043bd3c3083ae4c'
